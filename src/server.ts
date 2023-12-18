79f15b9dd1190c3a6a2cef5672712863266d0b90
AkisArou <akisarou90@gmail.com>
2023-12-18 16:41:21 +0200

    add pnpm support and autocompletion

--- src/server.ts
+++ src/server.ts
@@ -17,46 +17,85 @@ import {
 
 import * as tools from "workspace-tools";
 
+import fs from "fs";
+import path from "path";
+
+type WorkspacesType = "npm-yarn" | "pnpm";
+
 // Create a connection for the server, using Node's IPC as a transport.
 // Also include all preview / proposed LSP features.
-const connection = createConnection(ProposedFeatures.all);
 
+const workspacesType: WorkspacesType | null = (() => {
+  const workspaceRoot = tools.getWorkspaceRoot(process.cwd());
 
-// Create a simple text document manager.
-const documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
-
-connection.onInitialize((_: InitializeParams) => {
-  const result: InitializeResult = {
-    capabilities: {
-      textDocumentSync: TextDocumentSyncKind.Incremental,
-      // Tell the client that this server supports code completion.
-      completionProvider: {
-        resolveProvider: true
-      }
-    }
-  };
-
-  return result;
-});
-
-// This handler provides the initial list of the completion items.
-connection.onCompletion(
-  (_textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
-    const workspaces = tools.getWorkspaces(process.cwd())
-
-    return workspaces.map((w, idx) => ({
-      label: w.name,
-      kind: CompletionItemKind.File,
-      detail: w.path,
-      labelDetails: w.packageJson.description,
-      data: idx
-    }))
+  if (!workspaceRoot) {
+    return null;
   }
-);
 
-// Make the text document manager listen on the connection
-// for open, change and close text document events
-documents.listen(connection);
+  const parentPackageJSONPath = path.join(workspaceRoot, "package.json");
 
-// Listen on the connection
-connection.listen();
+  const parentPackageJSON = JSON.parse(
+    fs.readFileSync(parentPackageJSONPath, "utf-8")
+  );
+
+  if ("workspaces" in parentPackageJSON) {
+    return "npm-yarn"
+  }
+
+  const pnpmWorkspaceYamlExists = fs.existsSync(path.join(process.cwd(), "pnpm-workspace.yaml"))
+
+  if (pnpmWorkspaceYamlExists) {
+    return "pnpm"
+  }
+
+  return null
+})();
+
+if (workspacesType) {
+  init();
+}
+
+function init() {
+  const packageProtocol = workspacesType === "npm-yarn" ? "*" : "workspace:*"
+
+  const connection = createConnection(ProposedFeatures.all);
+  // Create a simple text document manager.
+  const documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
+
+  connection.onInitialize((_: InitializeParams) => {
+    const result: InitializeResult = {
+      capabilities: {
+        textDocumentSync: TextDocumentSyncKind.Incremental,
+        // Tell the client that this server supports code completion.
+        completionProvider: {
+          resolveProvider: true
+        }
+      }
+    };
+
+    return result;
+  });
+
+  // This handler provides the initial list of the completion items.
+  connection.onCompletion(
+    (_textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
+      const workspaces = tools.getWorkspaces(process.cwd())
+
+      return workspaces.map((w, idx) => ({
+        label: w.name,
+        kind: CompletionItemKind.File,
+        detail: w.path,
+        labelDetails: w.packageJson.description,
+        data: idx,
+        insertText: `"${w.name}": "${packageProtocol}"`
+      }))
+    }
+  );
+
+  // Make the text document manager listen on the connection
+  // for open, change and close text document events
+  documents.listen(connection);
+
+  // Listen on the connection
+  connection.listen();
+}
